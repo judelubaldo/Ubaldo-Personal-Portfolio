4a6b262277eb39fd17d2bef541c6986172b1875f
judelubaldo <judel.ubaldo@tup.edu.ph>
2024-11-08 20:46:13 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+INDEX
+
+<div id="contact">
+    <div class="container">
+        <div class="row">
+            <div class="contact-left">
+                <h1 class="sub-title">Contact Me</h1>
+                <p><i class="fa-solid fa-paper-plane"></i> judeubaldo20@gmail.com</p>
+                <p><i class="fa-solid fa-phone"></i> 09623357113</p>
+                <div class="social-icons">
+                    <a href="https://www.facebook.com/JEU0976/"><i class="fa-brands fa-facebook"></i></a>
+                </div>
+            </div>
+            <div class="contact-right">
+                <form>
+                    <input type="text" name="Name" placeholder="Your Name" required>
+                    <input type="email" name="email" placeholder="Your Email" required>
+                    <textarea name="Message" rows="6" placeholder="Your Message"></textarea>
+                    <button type="submit" class="btn btn2">Submit</button>
+                </form>
+            </div>
+        </div>
+    </dicv>
+</div>
